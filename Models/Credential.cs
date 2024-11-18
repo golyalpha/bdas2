446c5bb6b252b69aff4d78b4ce47cd922228bea4
Radek Goláň jr. <shield@shielddagger.com>
2024-11-18 14:22:58 +0100

    feat(db): rough demo of connecting model to DB

--- Models/Credential.cs
+++ Models/Credential.cs
@@ -1,8 +1,62 @@
 namespace WebApp.Models;
+using Oracle.ManagedDataAccess.Client; // ODP.NET Oracle managed provider
+using Oracle.ManagedDataAccess.Types;
+using WebApp.Util;
 
 public class Credential
 {
     public int Id { get; set; }
     public required string CredentialType { get; set; }
     public required string Data { get; set; }
+
+    public static Credential GetCredential(int Id)
+    {
+        using (OracleConnection conn = DBManager.GetConnection())
+        {
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT id_credential, data, credential_type FROM CREDENTIALS_V WHERE id_credential = :1";
+            cmd.Parameters.Add(Id);
+            cmd.CommandType = System.Data.CommandType.Text;
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                Credential cred = new Credential
+                {
+                    Id = reader.GetInt32(0),
+                    Data = reader.GetString(1),
+                    CredentialType = reader.GetString(2),
+                };
+                reader.Close();
+                conn.Close();
+                return cred;
+            }
+        }
+    }
+
+    public static List<Credential> ListCredentials() {
+        List<Credential> list = new List<Credential>();
+        using (OracleConnection conn = DBManager.GetConnection())
+        {
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT id_credential, data, credential_type FROM CREDENTIALS_V";
+            cmd.CommandType = System.Data.CommandType.Text;
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            { 
+                while (reader.Read())
+                {
+                    list.Add(new Credential
+                    {
+                        Id = reader.GetInt32(0),
+                        Data = reader.GetString(1),
+                        CredentialType = reader.GetString(2),
+                    });
+                }
+            }
+        }
+        
+        return list;
+    }
+
 }
---
+++ Util/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace WebApp.Util
+{
+    public class DBManager
+    {
+        public static OracleConnection GetConnection()
+        {
+            return new OracleConnection(System.Environment.GetEnvironmentVariable("DB_STRING"));
+        }
+    }
+}
