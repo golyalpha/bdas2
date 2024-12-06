4679f333646ced5eeabb4bc294f8dda6e026af7f
Radek Goláň jr. <shield@shielddagger.com>
2024-12-06 16:54:25 +0100

    forgot Persist in Role

--- Models/Role.cs
+++ Models/Role.cs
@@ -9,6 +9,24 @@ public class Role
     public int Id { get; set; }
     public required string Name { get; set; }
 
+    public void Persist()
+    {
+        using (OracleConnection conn = DBManager.GetConnection())
+        {
+            conn.Open();
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "edit_role";
+            cmd.Parameters.Add(Id);
+            cmd.Parameters.Add(Name);
+            int rows = cmd.ExecuteNonQuery();
+            if (rows == 0)
+            {
+                throw new ApplicationException("Persisting entity failed, no rows were updated.");
+            }
+        }
+    }
+
     public static Role GetRole(int Id)
     {
         using (OracleConnection conn = DBManager.GetConnection())
